5428c2efe3b709288dba7b75bbb0193f0b683d48
Jeff Pursell <jpursell@gmail.com>
2024-02-13 07:24:10 -0500

    Finish p12

--- p12/p12.jl
+++ p12/p12.jl
@@ -1,13 +1,14 @@
 
 using Combinatorics
 using Primes
+using Test
 
 function n_divisors(n)
     divisors = Set()
     for combo in combinations(factor(Vector, n))
         push!(divisors, prod(combo))
     end
-    return length(divisors) + 2
+    return length(divisors) + 1
 end
 
 @test n_divisors(1) == 1
@@ -17,3 +18,16 @@ end
 @test n_divisors(15) == 4
 @test n_divisors(21) == 4
 @test n_divisors(28) == 6
+
+function p12()
+    n = 1
+    while true
+        t = sum(1:n)
+        if n_divisors(t) > 500
+            return t
+        end
+        n += 1
+    end
+end
+
+p12()
