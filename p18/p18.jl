ed3859fb24cd83f70b1727378c53ebd4e3d52a86
Jeff Pursell <jpursell@gmail.com>
2024-02-15 06:54:40 -0500

    Finish p18

--- p18/p18.jl
+++ p18/p18.jl
@@ -1,3 +1,4 @@
+using Test
 
 function load_data()
     io = open("p18/data.txt")
@@ -12,4 +13,21 @@ function parse_data(lines)
     return data
 end
 
-parse_data(load_data())
+function find_max_path(data, irow, icol, tot)
+    tot += data[irow][icol]
+    if irow == length(data)
+        return tot
+    end
+    return max(
+        find_max_path(data, irow + 1, icol, tot),
+        find_max_path(data, irow + 1, icol + 1, tot)
+    )
+end
+
+function p18(data)
+    return find_max_path(data, 1, 1, 0)
+end
+
+@test p18([[3,],[7,4],[2,4,6],[8,5,9,3]]) == 23
+
+p18(parse_data(load_data()))
