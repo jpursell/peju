f46662bcea60392d255616ed151b27ceb7ffafa7
Jeff Pursell <jpursell@gmail.com>
2024-03-09 16:33:59 -0500

    Finish 24

--- 24/24.jl
+++ 24/24.jl
@@ -1,5 +1,12 @@
 # https://projecteuler.net/problem=24
+using Combinatorics
 
 function lex_orders(n)
-    
+    for t in permutations(0:n)
+        print(t)
+    end
 end
+
+lex_orders(2)
+
+nthperm(0:9, 1000000)
